9be627555e5906026aafdaf593d481d31b33a043
marta.carta <marta.carta@accenture.com>
2023-10-12 17:19:01 +0200

    aggiungo Logger

--- src/main/java/demo/HomeServlet.java
+++ src/main/java/demo/HomeServlet.java
@@ -1,6 +1,8 @@
 package demo;
 
 import java.io.IOException;
+import java.util.logging.Logger;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -13,18 +15,21 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/HomeServlet")
 public class HomeServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	private static Logger logger=Logger.getLogger(HomeServlet.class.getName());
        
     /**
      * @see HttpServlet#HttpServlet()
      */
     public HomeServlet() {
-        super();        
+        super(); 
+        logger.info("Costruisco HomeServlet");
     }
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {		
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		logger.info("Ricevuta get");
 		response.getWriter().append("Benvenuto");
 	}
 
@@ -32,6 +37,7 @@ public class HomeServlet extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		logger.info("Ricevuta post");
 		response.getWriter().append("Operazione non prevista");
 	}
 
